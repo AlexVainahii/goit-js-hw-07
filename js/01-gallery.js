c1cc96490779510619ef1402f5ac284b39953a2d
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-15 23:20:42 +0300

    1 finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+let galeryList = "";
+galleryItems.forEach((item) => {
+  const { preview, original, description } = item;
+  galeryList =
+    galeryList +
+    `<div class="gallery__item"><a class="gallery__link" href="${original}"> <img class="gallery__image"
+    src="${preview}" data-source="${original}" alt="${description}" /> </a> </div>`;
+});
+const mainGalery = document.querySelector(".gallery");
+mainGalery.insertAdjacentHTML("afterbegin", galeryList);
 console.log(galleryItems);
+
+mainGalery.addEventListener("click", selectImg);
+
+function selectImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img
+    src="${event.target.dataset.source}" height="600px" width="800px"  /> 
+    </div>`);
+  instance.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", (event) => {
+        console.log("hey");
+      });
+    }
+  });
+}
