2f7f668e885d833c6578da197aa70c1555cd829c
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-09-16 06:41:22 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+let galeryList = "";
+galleryItems.forEach((item) => {
+  const { preview, original, description } = item;
+  galeryList =
+    galeryList +
+    `<div class="gallery__item"><a class="gallery__link" href="${original}"> <img class="gallery__image"
+    src="${preview}" data-source="${original}" alt="${description}" /> </a> </div>`;
+});
+const mainGalery = document.querySelector(".gallery");
+mainGalery.insertAdjacentHTML("afterbegin", galeryList);
 console.log(galleryItems);
